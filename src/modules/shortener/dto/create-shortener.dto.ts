c677bba514c75f6d4554a887328d4afda3edd5cd
marlon <marlon@miflink.com>
2021-12-27 13:34:52 -0600

    shortener docs

--- src/modules/shortener/dto/create-shortener.dto.ts
+++ src/modules/shortener/dto/create-shortener.dto.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUrl } from 'class-validator';
 
 export class CreateShortenerDto {
+  @ApiProperty({
+    example: 'https://www.google.com.mx',
+  })
   @IsString()
   @IsUrl()
   url: string;
